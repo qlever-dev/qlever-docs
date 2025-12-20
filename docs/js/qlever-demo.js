b413589fe298cfad137311239069a17c17f9e3ff
Julian Mundhahs <julian.mundhahs@gmail.com>
2025-12-20 14:56:49 +0100

    cleanup

--- docs/js/qlever-demo.js
+++ docs/js/qlever-demo.js
@@ -7,21 +7,20 @@ document$.subscribe(function() {
 
 function mountRunDemoButton(el) {
     var code = el.querySelector('code')
+    var nav = el.querySelector('nav')
     var content = code.textContent.trim()
     var engine = el.getAttribute('data-demo-engine')
-    var parent = code.closest('pre')
 
     const button = renderRunDemoButton(engine, content)
-    parent.insertBefore(button, code)
+    nav.prepend(button)
 
 }
 
 function renderRunDemoButton(engine, operation) {
-  // Create the button element instead of returning an HTML string
   const button = document.createElement("a");
-  button.className = "md-run md-icon";
-  button.title = "Run";
-  button.setAttribute("href", `http://qlever.dev/${engine}?query=${encodeURIComponent(operation)}&exec=true`);
+  button.className = "md-run";
+  button.title = "Run query";
+  button.setAttribute("href", `https://qlever.dev/${engine}?query=${encodeURIComponent(operation)}&exec=true`);
   button.setAttribute("target", "_blank");
   button.setAttribute("rel", "noopener noreferrer");
 
